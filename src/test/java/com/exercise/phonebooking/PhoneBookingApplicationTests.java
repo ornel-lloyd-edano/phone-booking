f4ed539f28c677a1673901b00161a5fd3ba87a0a
ornel-lloyd-edano <ornel.lloyd.edano@gmail.com>
2023-05-28 07:26:15 +0400

    init project

---
+++ src/test/java/com/exercise/phonebooking/PhoneBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.phonebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
